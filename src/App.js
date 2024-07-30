200d6aab020e1a3ab46aef46f8e68d8aa32b6ea8
kevin kibebe <kibebekevin@gmail.com>
2024-07-30 18:43:19 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
 function App() {
+  const [input,setInput]=useState("")
+  const [todos,setTodos]=useState(new Set())
+  const addTodo=()=>{
+    if (input.trim === "") return;
+    setTodos((prevTodos)=>{
+      const newTodos=new Set([...prevTodos])
+      const isDuplicate=Array.from(newTodos).some(todo=>todo.text === input)
+      if(!isDuplicate){
+        newTodos.add({text:input,completed:false})
+      }else{
+        console.log('todo must be unique')
+      }
+      return newTodos;
+    })
+  }
+  const deleteTodo=(index)=>{
+    setTodos((prevTodos)=>{
+      const newTodos=new Set([...prevTodos])
+      const todosArray=Array.from(newTodos)
+      const todo=todosArray[index]
+      todosArray[index]={...todosArray[index],completed:!todo.completed}
+      return new Set(todosArray)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin:'auto'}}>
+        <input placeholder="Enter todo item"  onChange={(e)=>setInput(e.target.value)} value={input}/>
+        <button onClick={addTodo}>Add todo</button>
+        <br/>
+        <ul>
+        {Array.from(todos).map((todo, index) => (
+          <li key={index} style={{textDecoration:todo.completed ? "line-through": "none"}}>
+            {todo.text} {todo.completed ? "(Completed)" : ""}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
